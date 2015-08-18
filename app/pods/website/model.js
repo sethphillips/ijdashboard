a3feda1c71caaf92cbfa1b1656018c95986c1b3f
Seth Phillips <seth.phillips@trms.com>
2015-08-18 07:43:41 -0500

    oops

--- app/pods/website/model.js
+++ app/pods/website/model.js
@@ -19,8 +19,7 @@ Website.reopenClass({
 			id: 101,
 			title:'iJet.com',
 			url:'http://www.ijet.com',
-			username:'',
-			password:'',
+			
 			btn_img:'/img/btn_ijet_com.jpg',
 			placement:'top'
 		},
@@ -28,8 +27,7 @@ Website.reopenClass({
 			id: 102,
 			title:'Worldcue GCC Website Login',
 			url:'https://secure.worldcue.com/openssoDistauth/UI/Login?goto=https%3A%2F%2Fgcc.worldcue.com%2Fgroup%2Fworldcue',
-			username:'hannam',
-			password:'Password,5',
+			
 			btn_img:'img/btn_worldcue_gcc.jpg',
 			placement:'main'
 		},
@@ -37,8 +35,7 @@ Website.reopenClass({
 			id: 103,
 			title:'Worldcue Traveler Website Login',
 			url:'https://secure.worldcue.com/openssoDistauth/UI/Login?goto=https%3A%2F%2Ftraveler.worldcue.com%2Fdirector%2Fdirect',
-			username:'hannama',
-			password:'Password,5',
+			
 			btn_img:'img/btn_worldcue_traveler.jpg',
 			placement:'main'
 		},
@@ -46,8 +43,7 @@ Website.reopenClass({
 		// 	id: 104,
 		// 	title:'Critical Trac Website Login',
 		// 	url:'https://www.sicurotrack.com/Login.aspx',
-		// 	username:'',
-		// 	password:'',
+		// 	
 		// 	btn_img:'img/btn_critical_trac.jpg',
 		// 	placement:'main'
 		// },
@@ -55,8 +51,7 @@ Website.reopenClass({
 			id: 105,
 			title:'Worldcue Application Platform',
 			url:'https://gccaws.worldcue.com/wap/',
-			username:'clickmonkey',
-			password:'Password,52',
+			
 			btn_img:'img/btn_world_cue_2.jpg',
 			placement:'main'
 		},
@@ -64,8 +59,7 @@ Website.reopenClass({
 			id: 106,
 			title:'Daily Brief',
 			url:'http://www.ijet.com/node/1437',
-			username:'',
-			password:'',
+			
 			btn_img:'img/btn_daily_brief.jpg',
 			placement:'bottom'
 		},
@@ -73,8 +67,7 @@ Website.reopenClass({
 			id: 107,
 			title:'Health Monitor',
 			url:'http://www.ijet.com/node/1438',
-			username:'',
-			password:'',
+			
 			btn_img:'img/btn_health_monitor.jpg',
 			placement:'bottom'
 		},
@@ -82,8 +75,7 @@ Website.reopenClass({
 			id: 108,
 			title:'Monthly Forecast',
 			url:'http://www.ijet.com/node/1439',
-			username:'',
-			password:'',
+			
 			btn_img:'img/btn_monthly_forecast.jpg',
 			placement:'bottom'
 		},
